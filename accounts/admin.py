75e849b0f980cb9733e439734770cdf03dc3629c
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2026-01-21 10:20:04 +0500

    accounts apps

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import User
 
+
+admin.site.unregister(Group)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -10,9 +13,11 @@ class UserAdmin(admin.ModelAdmin):
         'first_name',
         'last_name',
         'phone_number',
+        'is_active',    # faollik holati
+        'is_superuser', # super foydalanuvchi huquqi
+        'is_staff',     # admin panelga kirish huquqi
         'is_worker',
         'is_manager',
-        'is_active'
     )
     search_fields = ('username', 'email', 'phone_number')
     list_filter = ('is_worker', 'is_manager', 'is_active')
