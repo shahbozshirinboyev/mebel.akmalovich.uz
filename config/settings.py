8673b65d2bd67c43035c7ef265c552d7276ffd89
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2026-02-16 18:09:41 +0500

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -99,8 +99,8 @@ AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ru'
+TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
 
